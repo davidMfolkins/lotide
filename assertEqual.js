f292429fe9c6b85185c3f9ac2f21ce83a6bd99b7
David Folkins <david.folkins88@gmail.com>
2020-09-14 18:44:54 +0000

    finished assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🚩🚩🚩 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+//TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("banana", "banana");
+assertEqual(23, 123);
