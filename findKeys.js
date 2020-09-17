2f9b32638ea1e6577b7263b53d7a26d47c70d63f
David Folkins <david.folkins88@gmail.com>
2020-09-17 23:54:56 +0000

    Finished findKeys.js

---
+++ findKeys.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚩🚩🚩 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = function(object, callback) {
+
+  for (let key in object) {
+    let star = object[key]
+    if (callback(star)) {
+      console.log(key)
+      return true
+    } 
+  }
+  return undefined
+};
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
