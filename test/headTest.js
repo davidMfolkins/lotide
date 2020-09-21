bc5db5ccdf245d94bdc10bf973fb2bb16e3b1b67
David Folkins <david.folkins88@gmail.com>
2020-09-21 13:18:27 -0400

    added gitignore, added mocha chai testing to headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,22 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
+describe("#head returns the first element of an array", () => {
+  
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), 5);
+  it("returns Hello for [Hello, Lighthouse, Labs]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+
+  it("returns 1 for [1]", () => {
+    assert.strictEqual(head([1]), 1);
+  });
+
+  it("returns [] for []", () => {
+    assert.strictEqual(head([]), );
+  });
+
+});
