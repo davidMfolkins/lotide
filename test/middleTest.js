a1648cd9127be9c9c77283523b560730a12a9ced
David Folkins <david.folkins88@gmail.com>
2020-09-21 13:28:56 -0400

    added mocha chai testing to middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,18 @@
-const assertArraysEqual = require("../assertArraysEqual")
-const middle = require("../middle")
+const assert = require('chai').assert;
+const head   = require('../middle');
 
+describe("#middle returns the middle elements of an array", () => {
+  
+  it("returns sheep, duck for [cow, sheep, duck, horse]", () => {
+    assert.deepEqual(head(["cow", "sheep", "duck", "horse"]), ["sheep", "duck"]);
+  });
 
-assertArraysEqual(middle(["cow", "sheep", "duck", "horse"]), ["sheep", "duck"]);
-assertArraysEqual(middle(["cow", "duck", "horse"]), ["duck"]);
-assertArraysEqual(middle(["cow", "sheep"]), []);
+  it("returns duck for [cow, duck, horse]", () => {
+    assert.deepEqual(head(["cow", "duck", "horse"]), ["duck"]); 
+  });
+
+  it("returns [] for [cow, sheep]", () => {
+    assert.deepEqual(head(["cow", "sheep"]), []);
+  });
+
+});
